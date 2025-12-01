fe9fb277c7799f4b0b88b96e1b5844452710735d
HHHHemanth <hemanth2102003acc2@gmail.com>
2025-12-01 22:16:35 +0530

    Updated project with new system changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 // app/layout.tsx  (root layout — KEEP THIS a server component, do NOT add "use client" here)
 import "./globals.css"; // <-- only place global CSS should be imported
 import React from "react";
+import { Roboto_Flex } from "next/font/google";
 
+const robotoFlex = Roboto_Flex({
+  subsets: ["latin"],
+});
 export const metadata = {
   title: "N&V Dashboard",
   description: "Dashboard",
@@ -9,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={robotoFlex.className}>
       <body>
         {children}
       </body>
